30c60473e36b265fcefec6836b505276f39ff2e8
fabianocamposcruz <fabiano.campos@ufrn.br>
2025-04-10 12:02:01 -0300

    atualização do fOutageCalculate

--- src/functions/fOutageCalculate.py
+++ src/functions/fOutageCalculate.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-def OutageCalculate (maskAllPoints, mtPowerFinaldBm, dSensitivity):
-    dOutRate = 100 * len(np.where(maskAllPoints & (mtPowerFinaldBm < dSensitivity))[0]) / np.sum(maskAllPoints)
-
-    return dOutRate
+def OutageCalculate(maskAllPoints, mtPowerFinaldBm, dSensitivity):
+    return (
+        100*len(np.where(maskAllPoints & (mtPowerFinaldBm < dSensitivity))[0]) / np.sum(maskAllPoints)
+    )
