dabc573fedee9942f3a0339385cb60ce6bc09953
Kaliane Marques <Kalianemarques@outlook.com>
2025-04-10 21:45:40 -0300

    adição de mais elementos a página e controle de visualização das informações de microcelula

--- public/module/index.js
+++ public/module/index.js
@@ -1,1 +1,29 @@
 // +controle dos elementos html e eventos
+
+const btnAdd = document.getElementById("show-microcelula");
+btnAdd.addEventListener("click", function () {
+    document.getElementById("hidden").style.display = "block";
+    //verifica se added-microcelula tem algum filho
+    const addedMicrocelula = document.getElementById("added-microcelula");
+    if (addedMicrocelula.children.length > 0) {
+        document.getElementById("delet-all-microcelula").style.display = "block";
+    } else {
+        document.getElementById("delet-all-microcelula").style.display = "none";
+    }
+
+});
+const btnClose = document.getElementById("close");
+btnClose.addEventListener("click", function () {
+    document.getElementById("hidden").style.display = "none";
+});
+const btnAddMicro = document.getElementById("add-microcelula");
+btnAddMicro.addEventListener('click', function () {
+    document.getElementById("delet-all-microcelula").style.display = "block";
+});
+// -criar função que gera os cards de microcelula
+
+// -criar função que deleta todos os cards de microcelula
+
+// -criar função que deleta um card de microcelula
+
+// -criar função que requisita os dados de outage e gráficos.
